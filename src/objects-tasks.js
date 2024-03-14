95a7bc5546b51226a3f1a713ab53a630fca418c6
Iurii <149243308+Metamorfoza42@users.noreply.github.com>
2024-03-14 18:30:47 +0400

    refactor: solve 4th task differently

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -85,14 +85,7 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
 function compareObjects(obj1, obj2) {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-  return keys1.every(
-    (key) => Object.hasOwn(obj2, key) && obj1[key] === obj2[key]
-  );
+  return Object.entries(obj1).toString() === Object.entries(obj2).toString();
 }
 
 /**
